336ff2a9450a18e773b8756b503caf79702b6483
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-08-23 22:55:35 -0300

    Retirado testes

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ var AWS = require("aws-sdk");
 var dynamodb = new AWS.DynamoDB({ region: process.env.AWS_DEFAULT_REGION });
 var docClient = new AWS.DynamoDB.DocumentClient({ service: dynamodb });
 
-var table = "students";
-
-var rm = "RM336648";
-
 const port = process.env.port || 5000;
 
 AWS.config.update({
@@ -21,28 +17,9 @@ AWS.config.update({
   "sessionToken" : process.env.AWS_SESSION_TOKEN,
 });
 
-var paramsInsert = {
-    TableName:table,
-    Item:{
-        "RM": "RM123456",
-        "age": "26",
-        "name": "Cidão",
-        "email": "teste@teste.com"
-    }
-};
-
 var params = {
     TableName: table
 };
-
-console.log("Adding a new item...");
-docClient.put(paramsInsert, function(err, data) {
-    if (err) {
-        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Added item:", JSON.stringify(data, null, 2));
-    }
-});
 
 app.listen(port, () => {
     console.log("Sever console log.")
