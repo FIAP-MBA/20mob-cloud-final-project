bc28c263cfb6a8a25318f56203489fc2ec26cc3a
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2020-08-23 21:54:46 -0300

    Adicionado get

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var app = express();
 
 var AWS = require("aws-sdk");
 
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+var table = "students";
+
+var rm = "RM336648";
+
+const port = process.env.port || 5000;
+
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION,
   endpoint: process.env.AWS_DEFAULT_ENDPOINT,
@@ -12,38 +20,23 @@ AWS.config.update({
   "sessionToken" : process.env.AWS_SESSION_TOKEN,
 });
 
-var dynamodb = new AWS.DynamoDB();
-
 var params = {
-  TableName : "Gostou",
-  KeySchema: [       
-      { AttributeName: "year", KeyType: "HASH"},  //Partition key
-      { AttributeName: "title", KeyType: "RANGE" }  //Sort key
-  ],
-  AttributeDefinitions: [       
-      { AttributeName: "year", AttributeType: "N" },
-      { AttributeName: "title", AttributeType: "S" }
-  ],
-  ProvisionedThroughput: {       
-      ReadCapacityUnits: 10, 
-      WriteCapacityUnits: 10
-  }
+    TableName: table,
+    Key:{
+        "RM": rm
+    }
 };
-
-dynamodb.createTable(params, function(err, data) {
-  if (err) {
-      console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-  } else {
-      console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-  }
-});
-
-app.get('/', function (req, res) {
-    res.send('GOSTOU');
-});
-
-const port = process.env.port || 5000;
 
 app.listen(port, () => {
     console.log("Sever console log.")
+});
+
+app.get('/', function (req, res) {
+    docClient.get(params, function(err, data) {
+        if (err) {
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        }
+    });
 });
